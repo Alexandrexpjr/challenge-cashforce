6e866779effabe0482bc3f23246871fe70e1db08
alexandrexpj <alexandrexpjr@hotmail.com>
2022-05-15 11:08:03 -0300

    feat: adiciona interface de `order`

---
+++ app/back-end/src/interface/IOrder.ts
@@ -0,0 +1,32 @@
+export interface IOrder {
+  id?: number,
+  orderNfId?: string,
+  orderNumber?: string,
+  orderPath?: string,
+  orderFileName?: string,
+  orderOriginalName?: string,
+  emissionDate?: string,
+  pdfFile?: string,
+  emitedTo?: string,
+  nNf?: string,
+  CTE?: string,
+  value?: string,
+  createdAt?: Date,
+  updatedAt?: Date,
+  cnpjId?: number,
+  userId?: number,
+  buyerId?: number,
+  providerId?: number,
+  orderStatusBuyer?: string,
+  orderStatusProvider?: string,
+  deliveryReceipt?: string,
+  cargoPackingList?: string,
+  deliveryCtrc?: string
+}
+
+export interface IOrderWithData extends IOrder {
+  cnpj?: string,
+  userName?: string,
+  buyerName?: string,
+  providerName?: string,
+}
